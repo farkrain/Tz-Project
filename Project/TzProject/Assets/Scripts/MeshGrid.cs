ab15670d59cc083bae109f0e8a6b0abf2989003e
farkrain <farkrain@yandex.ru>
2016-01-05 01:44:08 +0300

    запилена активация клетки

---
+++ Project/TzProject/Assets/Scripts/MeshGrid.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshGrid : MonoBehaviour {
+	public GameObject targetGrid;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+/*
+void Start()
+{
+	gameObject.AddComponent("MeshFilter");
+	Mesh mesh = GetComponent<MeshFilter>().mesh;
+	
+	mesh.vertices = new Vector3[]
+	{
+		new Vector3(0, 0, 0),
+		new Vector3(1.9f, 0, 0),
+		new Vector3(1.9f, 1.9f, 0),
+		new Vector3(0, 0, 1),
+		new Vector3(1.9f, 0, 1),
+		new Vector3(1.9f, 1.9f, 1)
+	};
+	mesh.triangles = new int[] { 0, 1, 2, 3, 4, 5, 0, 3, 2, 3, 2, 5, 0, 3, 4, 0, 1, 4, 2, 4, 5, 1, 2, 4 };
+	
+	transform.GetComponent<MeshCollider>().sharedMesh = mesh;
+}
+}
+*/
--- Project/TzProject/Assets/Scripts/TargetGrid.cs
+++ Project/TzProject/Assets/Scripts/TargetGrid.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class TargetGrid : MonoBehaviour {
 	public Camera targetCamera;
+	public Sprite Grid;
+	public Sprite ActiveGrid;
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0))
@@ -11,8 +13,18 @@ public class TargetGrid : MonoBehaviour {
 			RaycastHit hit;
 			if (Physics.Raycast(ray, out hit, 1000))
 			{
-				string obj = hit.collider.gameObject.name;
-
+				GameObject obj = hit.collider.gameObject;
+				obj.GetComponent<SpriteRenderer>().sprite=ActiveGrid;
+			}
+		}
+		if (Input.GetMouseButtonUp(0))
+		{
+			Ray ray = targetCamera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 1000))
+			{
+				GameObject obj = hit.collider.gameObject;
+				obj.GetComponent<SpriteRenderer>().sprite=Grid;
 			}
 		}
 	}
