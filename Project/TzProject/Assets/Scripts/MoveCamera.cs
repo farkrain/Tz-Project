6a737b7c3eb9fe7988b5977971072aec03841b90
farkrain <farkrain@yandex.ru>
2016-01-04 11:50:04 +0300

    Запилил чаров и камеру

---
+++ Project/TzProject/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+	public Camera targetCamera;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			targetCamera.transform.position += new Vector3 (Input.GetAxis ("Mouse Y"), 0, -Input.GetAxis ("Mouse X")) * Time.deltaTime * 5;
+			targetCamera.transform.position = new Vector3 (Mathf.Clamp (targetCamera.transform.position.x, -0.08F, 0.5F), targetCamera.transform.position.y, Mathf.Clamp (targetCamera.transform.position.z, -2.2F, 2.2F));
+		}
+	}
+}
