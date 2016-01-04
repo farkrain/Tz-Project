6ad21255f2d21f6dbb3d6ccbdaaac1c7632d9c57
farkrain <farkrain@yandex.ru>
2016-01-04 07:21:34 +0300

    Перенастроил камеру, создал сетку

--- Project/TzProject/Assets/Scripts/SetGrid.cs
+++ Project/TzProject/Assets/Scripts/SetGrid.cs
@@ -2,10 +2,25 @@
 using System.Collections;
 
 public class SetGrid : MonoBehaviour {
-
+	public GameObject Grid;
 	// Use this for initialization
 	void Start () {
-	
+		for (int i=0; i<26; i++) {
+			for (int j=0; j<65; j++){
+				GameObject grid = Instantiate(Grid) as GameObject;
+				if (i%2==0)
+				{
+					grid.GetComponent<Transform>().position = new Vector3((-1.25F + (i*0.074F)),0.001F,(-3.2F + (j*0.098F)));
+					grid.name = (i+1).ToString()+ "," + (j+1).ToString();
+				}
+				else
+				{
+					grid.GetComponent<Transform>().position = new Vector3((-1.25F + (i*0.074F)),0.001F,(-3.15F + (j*0.098F)));
+					grid.name = (i+1).ToString()+ "," + (j+1).ToString();
+				}
+			}
+		}
+
 	}
 	
 	// Update is called once per frame
