38859c45e06e27ec49705612b8691587cb08a2e6
farkrain <farkrain@yandex.ru>
2016-01-05 06:12:32 +0300

    Почистил проект

---
+++ Project/TzProject/Assets/Scripts/ConnectServer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+public class ConnectServer : MonoBehaviour 
+{
+	public string res;
+	void Start ()
+	{ 
+		ConnectClient();
+	}
+	void ConnectClient()
+	{
+		TcpClient tcpClient = new TcpClient();
+		tcpClient.Connect ("cr0.nz", 25564);
+		NetworkStream stream = tcpClient.GetStream ();
+		StreamWriter strwrite = new StreamWriter (stream);
+		StreamReader strread = new StreamReader (stream);
+		strwrite.WriteLine ("123");
+		strwrite.Flush ();
+		Debug.Log ("Логин отправлен");
+		res = strread.ReadLine ();
+		Debug.Log(res.ToString());
+
+	}
+}
