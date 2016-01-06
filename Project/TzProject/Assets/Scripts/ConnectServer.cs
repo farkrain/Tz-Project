0f5269a13d1fc56a25bd946ccf6efd6d908c2be1
farkrain <farkrain@yandex.ru>
2016-01-06 05:44:09 +0300

    починил

--- Project/TzProject/Assets/Scripts/ConnectServer.cs
+++ Project/TzProject/Assets/Scripts/ConnectServer.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.Serialization;
 using Newtonsoft.Json;
 
 public class ConnectServer : MonoBehaviour 
